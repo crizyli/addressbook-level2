5c225f369bf4368a1c8297d50b26258ed534a642
IMAC-SL\imac <e0134315@u.nus.edu>
2018-02-11 12:30:14 +0800

    implement inheritance

--- src/seedu/addressbook/data/person/Address.java
+++ src/seedu/addressbook/data/person/Address.java
@@ -6,19 +6,12 @@ import seedu.addressbook.data.exception.IllegalValueException;
  * Represents a Person's address in the address book.
  * Guarantees: immutable; is valid as declared in {@link #isValidAddress(String)}
  */
-public class Address {
+public class Address extends Contact{
 
-    public static final String EXAMPLE = "123, Clementi Ave 3, #12-34, 231534";
-    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Address should be 4 strings seperated by commas";
-    public static final String ADDRESS_VALIDATION_REGEX = ".+, .+, .+, .+";
+    public static final String EXAMPLE = "123, some street";
+    public static final String MESSAGE_ADDRESS_CONSTRAINTS = "Person addresses can be in any format";
+    public static final String ADDRESS_VALIDATION_REGEX = ".+";
 
-    public final Block block;
-    public final Street street;
-    public final Unit unit;
-    public final PostalCode postalCode;
-
-    public final String value;
-    private boolean isPrivate;
 
     /**
      * Validates given address.
@@ -26,20 +19,12 @@ public class Address {
      * @throws IllegalValueException if given address string is invalid.
      */
     public Address(String address, boolean isPrivate) throws IllegalValueException {
+        super(address, isPrivate);
         String trimmedAddress = address.trim();
-        this.isPrivate = isPrivate;
         if (!isValidAddress(trimmedAddress)) {
             throw new IllegalValueException(MESSAGE_ADDRESS_CONSTRAINTS);
         }
 
-        String[] addressElements = trimmedAddress.split(",");
-
-        this.block = new Block(addressElements[0].trim());
-        this.street = new Street(addressElements[1].trim());
-        this.unit = new Unit(addressElements[2].trim());
-        this.postalCode = new PostalCode(addressElements[3].trim());
-
-        this.value = trimmedAddress;
     }
 
     /**
@@ -49,10 +34,6 @@ public class Address {
         return test.matches(ADDRESS_VALIDATION_REGEX);
     }
 
-    @Override
-    public String toString() {
-        return value;
-    }
 
     @Override
     public boolean equals(Object other) {
@@ -61,12 +42,4 @@ public class Address {
                 && this.value.equals(((Address) other).value)); // state check
     }
 
-    @Override
-    public int hashCode() {
-        return value.hashCode();
-    }
-
-    public boolean isPrivate() {
-        return isPrivate;
-    }
 }
